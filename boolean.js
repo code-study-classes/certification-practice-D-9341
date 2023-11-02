826da8f59c5e8032cb6154107aea4f8f63af1c7c
Даниил <wsx86932321@yandex.ru>
2023-11-02 09:17:37 +0300

    task-2

--- boolean.js
+++ boolean.js
@@ -2,3 +2,8 @@ export const isPositive = (numby) => {
     if (numby === 0) return false
     return true ? numby === Math.abs(numby) : false
 }
+
+export const isOdd = (numby) => {
+    if (numby === 0) return false
+    return true ? numby % 2 !== 0 : false
+}
