8a7a401efb304ceccd4f737d8b4e46c76adfb7c7
Даниил <wsx86932321@yandex.ru>
2023-11-02 09:19:19 +0300

    task-3

--- boolean.js
+++ boolean.js
@@ -7,3 +7,7 @@ export const isOdd = (numby) => {
     if (numby === 0) return false
     return true ? numby % 2 !== 0 : false
 }
+
+export const checkInequality = (a, b) => {
+    return true ? a > 2 && b <= 3 : false
+}
